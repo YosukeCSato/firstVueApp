3108dc50afe5e93c4e5c500b6771074d108a018d
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-11-11 19:48:57 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var items = [
+  {
+    name: '鉛筆',
+    price: 300,
+    quantity: 0,
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 0,
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 0,
+  },
+];
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    // dataプロパティ
+    items: items,
+    loggedInButton: 'ログイン済みのため購入できます',
+    canBuy: false,
+  },
+  filters: {
+    // この節で追加したフィルタの定義
+    numberWithDelimiter: function(value) {
+      if (!value) {
+        return 0;
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    },
+  },
+});
