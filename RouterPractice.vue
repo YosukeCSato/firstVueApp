81e95e250c2122adc64ba71c1ac185df854cd2f7
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-11-16 23:54:20 +0900

    やりなおし

---
+++ RouterPractice.vue
@@ -0,0 +1,23 @@
+<template> </template>
+
+<script lang="js">
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/top',
+      component: {
+        template: '<h1>トップページです。</h1>'
+      }
+    },
+    {
+      path: '/users',
+      component: {
+        template: '<div>ユーザー一覧ページです。'
+      }
+    }
+  ]
+})
+</script>
+
+<style lang="scss" module>
